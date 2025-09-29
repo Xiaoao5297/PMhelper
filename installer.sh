1fc21022d02312b68e3b7cbe15687c7419b31502
Xiaoao5297 <2574459918@qq.com>
2025-09-29 21:42:38 +0800

    更新installer.sh,修复小bug

--- installer.sh
+++ installer.sh
@@ -17,26 +17,27 @@ PMMP_API="2.1.0"
 PMMP_PHP="php7.0~7.3"
 PHP_VER="7.2.8"
 
-read -p "[*]欢迎来到这个安装程序(回车继续)"
-read -p "[*]需要了解这个安装程序所用的PocketMine版本和php版本吗？(y/N)" choice
+# 显示欢迎信息
+echo "[*]欢迎来到这个安装程序"
+read -p "按回车继续..." 
 
-choice_upper=$(echo "$choice" | tr '[:lower:]' '[:upper:]')
+# 询问用户是否需要查看版本信息
+read -p "[*]需要了解这个安装程序所用的PocketMine版本和php版本吗？(y/N) " choice
 
-case $choice_upper in
-    y)
+# 处理用户输入
+case "$choice" in
+    [Yy]*)  # 匹配以 Y 或 y 开头的任何输入
         echo "PocketMine版本: $PMMP_NAME $PMMP_VER API $PMMP_API"
         echo "PocketMine支持的php版本: $PMMP_PHP"
         echo "此安装程序所用版本: $PHP_VER"
+        read -p "按回车键开始安装..." 
         ;;
-    n)
-        echo "即将开始安装程序..."
-        sleep 1
-        ;;
-    *)
+    *)  # 所有其他情况（包括空输入）
         echo "即将开始安装程序..."
         sleep 1
         ;;
 esac
+
 # 1. 清理旧文件（如果存在）
 echo "[*]正在清理旧文件..."
 rm -f "$TEMP_ZIP"
